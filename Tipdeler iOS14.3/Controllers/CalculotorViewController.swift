34e67b6be2e36615d137da8d22cfac4fa7a37446
hansAGB <hans.aangeenbrug@icloud.com>
2021-01-03 21:38:31 +0100

    New app Icon, some code in Viewcontroller, works without seconds view

--- Tipdeler iOS14.3/Controllers/CalculotorViewController.swift
+++ Tipdeler iOS14.3/Controllers/CalculotorViewController.swift
@@ -13,16 +13,61 @@ class CalculotorViewController: UIViewController {
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
-    @IBOutlet weak var spitNumberLabel: UILabel!
+    @IBOutlet weak var splitNumberLabel: UILabel!
     
+    var tipPerc: Float = 0.0
+    var numPeople: Int = 2
+    var billTotal: Float = 0.0
+    var deelBedrag: Float = 0.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
 
     @IBAction func tipChanged(_ sender: UIButton) {
+        
+        billTextField.endEditing(true)
+        billTotal = Float(billTextField.text ?? "0.0")!
+        
+        deselectButtons()
+        sender.isSelected = true
+        
+        let pctTip = sender.currentTitle ?? "0.0"
+        if pctTip == "0%" {
+            tipPerc = 0.0
+        } else if pctTip == "10%" {
+            tipPerc = 0.1
+        } else if pctTip == "20%" {
+            tipPerc = 0.2
+        }
+    
+        print(String(format: "%.1f", tipPerc))
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        print(sender.value)
+        numPeople = Int(sender.value)
+        splitNumberLabel.text = String(numPeople)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        print("Aantal mensen is \(numPeople)")
+        
+        print("Het te betalen bedrag is \(billTotal)")
+        
+        print("De gekozen TIP is \(tipPerc)")
+        
+        deelBedrag = billTotal * (1.0 + tipPerc) / Float(numPeople)
+        
+        print("De uitkomst is \(String(format: "%.2f", deelBedrag))")
+    }
+    
+    func deselectButtons() {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
     }
 }
 
