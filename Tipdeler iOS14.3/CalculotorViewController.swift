c6c879e473d56ac3ce4cb5e63bc0869874d0a3d7
hansAGB <hans.aangeenbrug@icloud.com>
2021-01-03 19:30:49 +0100

    IBOutlets and IBActions added

---
+++ Tipdeler iOS14.3/CalculotorViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Tipdeler iOS14.3
+//
+//  Created by Hans Aangeenbrug on 03/01/2021.
+//
+
+import UIKit
+
+class CalculotorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var spitNumberLabel: UILabel!
+    
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+}
+
